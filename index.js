4d356a851ae098c314bcb51931ca34485ede8fdf
Seven <seven@megalive.com.tw>
2017-04-16 19:01:16 +0800

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+function serialGen(generator) {
+  var gen = generator();
+
+  if (typeof gen.next !== 'function') {
+    throw Error('You must provide the Generator function.');
+  }
+
+  function process(iteratorObj) {
+    var done = iteratorObj.done;
+    var value = iteratorObj.value;
+    var promiseKeys;
+    var valArr;
+
+    if (done) return Promise.resolve(value);
+
+    // yield an array
+    if (value.constructor.name === 'Array') {
+      return Promise.all(value);
+    }
+
+    // yield Promise within object
+    if (value.constructor.name === 'Object') {
+      promiseKeys = [];
+      valArr = Object.keys(value).map(function (v) {
+        promiseKeys.push(v);
+        return value[v];
+      });
+
+      return Promise.all(valArr).then(function (computedArr) {
+        var resultObj = {};
+
+        computedArr.forEach(function (computedVal, i) {
+          resultObj[promiseKeys[i]] = computedVal;
+        });
+        return resultObj;
+      });
+    }
+
+    // yield a Promise
+    if (typeof value.then === 'function') {
+      return value.then(function (result) {
+        return process(gen.next(result));
+      });
+    }
+
+    // otherwise
+    return process(gen.next(value));
+  }
+
+  return process(gen.next());
+}
+
+module.exports = serialGen;
---
+++ test.js
@@ -0,0 +1,84 @@
+import ava from 'ava';
+import sg from './';
+
+ava('yield pure Promise', t =>
+  sg(function* () {
+    return yield Promise.resolve('promise');
+  }).then((result) => {
+    t.is(result, 'promise');
+  }),
+);
+
+ava('yield primitives type', t =>
+  sg(function* () {
+    return yield 'primitives';
+  }).then((result) => {
+    t.is(result, 'primitives');
+  }),
+);
+
+ava('yield Function', t =>
+  sg(function* () {
+    const fn = () => 'function';
+    return yield fn();
+  }).then((result) => {
+    t.deepEqual(result, 'function');
+  }),
+);
+
+ava('yield Generator', t =>
+  sg(function* () {
+    const gen = function* () {
+      const result1 = yield 'generator1';
+      const result2 = yield 'generator2';
+      return [result1, result2];
+    };
+    return yield* gen();
+  }).then((result) => {
+    t.deepEqual(result, ['generator1', 'generator2']);
+  }),
+);
+
+ava('yield array of Promise 1', t =>
+  sg(function* () {
+    const result1 = yield Promise.resolve('array1');
+    const result2 = yield Promise.resolve('array2');
+    return [result1, result2];
+  }).then((result) => {
+    t.deepEqual(result, ['array1', 'array2']);
+  }),
+);
+
+ava('yield array of Promise 2', t =>
+  sg(function* () {
+    const a = Promise.resolve('array1');
+    const b = Promise.resolve('array2');
+    const c = Promise.resolve('array3');
+    return yield [a, b, c];
+  }).then((result) => {
+    t.deepEqual(result, ['array1', 'array2', 'array3']);
+  }),
+);
+
+ava('yield object of Promise', t =>
+  sg(function* () {
+    yield {
+      1: Promise.resolve('object1'),
+      2: Promise.resolve('object2'),
+    };
+  }).then(result => t.deepEqual(result, { 1: 'object1', 2: 'object2' })),
+);
+
+ava('should throw error when reject by Promise', t =>
+  sg(function* () {
+    yield Promise.reject(new Error('boom'));
+  }).catch(err => t.is(err.message, 'boom')),
+);
+
+ava('should throw error if dose not receive a Generator function', (t) => {
+  const fn = () => sg(() => 'Throw an Error');
+  const error = t.throws(() => {
+    fn();
+  }, Error);
+  t.is(error.message, 'You must provide the Generator function.');
+});
